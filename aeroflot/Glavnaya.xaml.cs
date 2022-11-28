4269cdfb078ec52129cab1d62cd532a12fb5c023
Dima Sorokin <sorokindima36@gmail.com>
2022-11-28 12:47:54 +0300

    f

--- aeroflot/Glavnaya.xaml.cs
+++ aeroflot/Glavnaya.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,12 +23,26 @@ namespace aeroflot
     /// </summary>
     public partial class Glavnaya : Page
     {
+        public static DateTime Today { get; }
         string User;
         public Glavnaya(string user)
         {
             InitializeComponent();
             DgridHotels2.Visibility = Visibility.Collapsed;
-            DgridHotels.ItemsSource = Entities.GetContext().reis.ToList();
+             
+            List<aeroflot.reis> reisii = new List<aeroflot.reis>();
+            reisii = Entities.GetContext().reis.ToList();
+            int counts = Entities.GetContext().reis.Count();
+            for (int i = 0; i < counts; i++)
+            {
+                if (reisii[i].date < DateTime.Now)
+                {
+                    reisii.RemoveAt(i);
+                    i--;
+                    counts--;
+                }
+            }
+            DgridHotels.ItemsSource = reisii;
             Glavnaya customer = (Glavnaya)DgridHotels.SelectedItem;
             DgridHotels1.ItemsSource = Entities.GetContext().planes.ToList();
             Glavnaya customer1 = (Glavnaya)DgridHotels1.SelectedItem;
@@ -99,40 +114,66 @@ namespace aeroflot
             DgridHotels2.ItemsSource = user;
             Glavnaya customer2 = (Glavnaya)DgridHotels2.SelectedItem;
         }
-
+        public int rel = 0;
         private void DgridHotels_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (MessageBox.Show($"Вы хотите забронировать этот рейс?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            if(rel == 0)
             {
-                List<aeroflot.reis> r = new List<aeroflot.reis>();
-                r = Entities.GetContext().reis.ToList();
-                int count11 = Entities.GetContext().reis.Count();
-                int count111 = Entities.GetContext().klients.Count();
-                IList rows = DgridHotels.SelectedItems;
-                object a = rows[0];
-                for (int i = 0; i < count11; i++)
+                if (MessageBox.Show($"Вы хотите забронировать этот рейс?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
                 {
-                    if (r[i].reis1 == ((aeroflot.reis)a).reis1)
+                    List<aeroflot.reis> r = new List<aeroflot.reis>();
+                    r = Entities.GetContext().reis.ToList();
+                    int count11 = Entities.GetContext().reis.Count();
+                    int count111 = Entities.GetContext().klients.Count();
+                    IList rows = DgridHotels.SelectedItems;
+                    object a = rows[0];
+                    for (int i = 0; i < count11; i++)
                     {
-                        int index = r[i].id;
-                        List<aeroflot.pokupki> rr = new List<aeroflot.pokupki>() { new pokupki() };
-                        List<aeroflot.klients> rrr = new List<aeroflot.klients>() ;
-                        rrr = Entities.GetContext().klients.ToList();
-                        rr[0].id_reis = index;
-                        for (int j = 0; j < count11; j++)
+                        if (r[i].reis1 == ((aeroflot.reis)a).reis1)
                         {
-                            if (rrr[j].klient==User)
+                            int index = r[i].id;
+                            List<aeroflot.pokupki> rr = new List<aeroflot.pokupki>() { new pokupki() };
+                            List<aeroflot.klients> rrr = new List<aeroflot.klients>();
+                            rrr = Entities.GetContext().klients.ToList();
+                            rr[0].id_reis = index;
+                            for (int j = 0; j < count11; j++)
                             {
-                                rr[0].id_klient = rrr[j].id;
-                                break;
+                                if (rrr[j].klient == User)
+                                {
+                                    rr[0].id_klient = rrr[j].id;
+                                    break;
+                                }
                             }
+                            rr[0].status = "Активен";
+                            Entities.GetContext().pokupki.Add(rr[0]);
+                            Entities.GetContext().SaveChanges();
+                            rel = 1;
                         }
-                        rr[0].status = "Активен";
-                        Entities.GetContext().pokupki.Add(rr[0]);
-                        Entities.GetContext().SaveChanges();
                     }
                 }
+                else
+                {
+                    rel = 1;
+                }
             }
+           
+            DgridHotels.ItemsSource = null;
+            List<aeroflot.reis> reisiii = new List<aeroflot.reis>();
+            reisiii = Entities.GetContext().reis.ToList();
+            int counts1 = Entities.GetContext().reis.Count();
+            for (int j = 0; j < counts1; j++)
+            {
+                if (reisiii[j].date < DateTime.Now)
+                {
+                    reisiii.RemoveAt(j);
+                    j--;
+                    counts1--;
+                }
+            }
+            DgridHotels.ItemsSource = reisiii;
+            Glavnaya customer = (Glavnaya)DgridHotels.SelectedItem;
+            rel = 0;
         }
-    }
+    
+    }   
 }
